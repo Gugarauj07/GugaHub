a1cf49afec9169a88db9f25a1236e253827d4c77
unknown <gugaraujo01@gmail.com>
2023-07-16 23:03:58 -0400

    theme

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
@@ -11,17 +16,20 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
             <Route
               path="/profile/:userId"
-              element={<ProfilePage />}
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
             />
           </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
