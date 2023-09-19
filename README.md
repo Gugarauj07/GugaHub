5d2c64c2281fb353fa1270898915e6382e38c186
Gugarauj07 <gugaraujo01@gmail.com>
2023-09-19 16:35:30 -0300

    widgets

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GugaHub
-Welcome to GugaHub, a cutting-edge social media web application built using the power of the MERN (MongoDB, Express.js, React.js, Node.js) stack, combined with the robustness and type safety of TypeScript.
+Welcome to GugaHub, a cutting-edge social media web application built using the power of the MERN (MongoDB, Express.js, React.js, Node.js) stack.
--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route
               path="/home"
-              // element={isAuth ? <HomePage /> : <Navigate to="/" />}
-              element={<HomePage />}
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+              // element={<HomePage />}
             />
             <Route
               path="/profile/:userId"
